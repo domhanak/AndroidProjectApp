5317c2a8887545a0fe2a19cf853938752c79cf2e
domhanak <domin.hanak@gmail.com>
2015-10-11 20:40:54 +0200

    task03: Add themes.xml and use GridView in MainActivity

--- app/src/main/java/cz/muni/fi/pv256/movio/uco410430/MainActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco410430/MainActivity.java
@@ -4,14 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+      //  gridview.setAdapter(new ArrayAdapter<String>(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "Hello " + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
