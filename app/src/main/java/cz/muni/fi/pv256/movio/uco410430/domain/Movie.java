d7a7f47cf67164a9dffe0b9bd306161f687d9f57
domhanak <domin.hanak@gmail.com>
2015-11-09 08:37:50 +0100

    task05: make Movie implement Parcelable

--- app/src/main/java/cz/muni/fi/pv256/movio/uco410430/domain/Movie.java
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco410430/domain/Movie.java
@@ -1,11 +1,14 @@
 package cz.muni.fi.pv256.movio.uco410430.domain;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Simple domain class representing Movie.
  *
  * @author Dominik Hanak
  */
-public class Movie {
+public class Movie implements Parcelable {
     private long releaseDate;
     private String coverPath;
     private String title;
@@ -42,4 +45,32 @@ public class Movie {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(this.releaseDate);
+        dest.writeString(this.coverPath);
+        dest.writeString(this.title);
+    }
+
+    private Movie(Parcel in) {
+        this.releaseDate = in.readLong();
+        this.coverPath = in.readString();
+        this.title = in.readString();
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
