dfa3a637b996bccea6570150a998fde8642f4d0b
domhanak <domin.hanak@gmail.com>
2015-12-13 15:05:16 +0100

    task08: add Api and DownloadService

---
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco410430/network/Api.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.pv256.movio.uco410430.network;
+
+import java.util.List;
+
+import cz.muni.fi.pv256.movio.uco410430.domain.Movie;
+import cz.muni.fi.pv256.movio.uco410430.utils.Constants;
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Interface for rest api.
+ *
+ * Created by dhanak on 11/30/15.
+ */
+public interface Api {
+    @GET(Constants.POPULAR_URL + Constants.API_KEY)
+    void getMovies(Callback<Responses.LoadMovieResponse> response);
+
+    @GET("/movie/{id}/credits" + Constants.API_KEY)
+    void getCast(@Path("id") int id, Callback<Responses.LoadMovieResponse> response);
+}
---
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco410430/service/MovieDownloadService.java
@@ -0,0 +1,70 @@
+package cz.muni.fi.pv256.movio.uco410430.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.List;
+
+import cz.muni.fi.pv256.movio.uco410430.App;
+import cz.muni.fi.pv256.movio.uco410430.domain.Movie;
+import cz.muni.fi.pv256.movio.uco410430.network.Api;
+import cz.muni.fi.pv256.movio.uco410430.network.Responses;
+import cz.muni.fi.pv256.movio.uco410430.utils.Constants;
+import de.greenrobot.event.EventBus;
+import retrofit.Callback;
+import retrofit.RequestInterceptor;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.OkClient;
+import retrofit.client.Response;
+
+/**
+ * Service for downloading Movies.
+ *
+ * Created by dhanak on 11/30/15.
+ */
+public class MovieDownloadService extends IntentService {
+
+    public MovieDownloadService() {
+        super("MovieDownloadService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.i("MovieDownloadService" , "onHandleIntent");
+
+        App app = (App) getApplicationContext();
+        RestAdapter adapter =
+                new RestAdapter.Builder()
+                        .setEndpoint(Constants.BASE_URL)
+                        .setClient(new OkClient())
+                        .setLogLevel(RestAdapter.LogLevel.FULL)
+                        .setLog(new RestAdapter.Log() {
+                            @Override
+                            public void log(String message) {
+                                Log.d("RETROFIT", message);
+                            }
+                        }).setRequestInterceptor(new RequestInterceptor() {
+                            @Override
+                        public void intercept(RequestFacade request) {
+                                request.addHeader("Accept", "application/json");
+                            }
+                        }).build();
+
+
+        Api api = adapter.create(Api.class);
+        api.getMovies(new Callback<Responses.LoadMovieResponse>() {
+                    @Override
+                    public void success(final Responses.LoadMovieResponse loadFilmsResponse, final Response response) {
+                        Log.d("Download - movie:",loadFilmsResponse.mMovies.get(0).getTitle());
+                        EventBus.getDefault().post(new Responses.LoadMovieResponse(loadFilmsResponse.mMovies));
+                    }
+
+                    @Override
+                    public void failure(final RetrofitError error) {
+                        Log.d("DownloadService", error.toString());
+                    }
+        });
+    }
+}
