357ef0da5a3697db289c3fdd8dd5a02638ff5954
domhanak <domin.hanak@gmail.com>
2015-12-13 15:06:18 +0100

    task08: add one response to Responses for Movie

--- app/src/main/java/cz/muni/fi/pv256/movio/uco410430/network/Responses.java
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco410430/network/Responses.java
@@ -1,7 +1,26 @@
 package cz.muni.fi.pv256.movio.uco410430.network;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import cz.muni.fi.pv256.movio.uco410430.domain.Movie;
+
 /**
+ * Class defining response for movies.
+ *
  * Created by dhanak on 11/30/15.
  */
 public class Responses {
+
+    private Responses() {}
+
+    public static class LoadMovieResponse {
+        @SerializedName("results")
+        public List<Movie> mMovies;
+
+        public LoadMovieResponse(final List<Movie> movies) {
+            mMovies = movies;
+        }
+    }
 }
